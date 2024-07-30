06c75c116fe03d2db274f9e50066c94e9a629322
EbrahimTahernejad <EbrahimTahernejad@users.noreply.github.com>
2024-07-30 02:52:38 +0000

    Libcore Release 0.0.4

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ import PackageDescription
 
 let package = Package(
   name: "Libcore",
-  platforms: [.iOS(.v12)],
+  platforms: [.iOS(.v12), .macOS(.v10_13)],
   products: [
     .library(name: "Libcore", targets: ["Libcore"])
   ],
   targets: [
     .binaryTarget(
       name: "Libcore",
-      url: "https://github.com/EbrahimTahernejad/hiddify-next-core-lib/releases/download/0.0.3/Libcore.xcframework.zip",
-      checksum: "d715c3c2fda09c6633aad465492497f872a2401aa62c050b6d110e0de4e5ae3a"
+      url: "https://github.com/EbrahimTahernejad/hiddify-next-core-lib/releases/download/0.0.4/Libcore.xcframework.zip",
+      checksum: "e9735bb9adf91e2b4dfd3590333dbb15a6c57b8bbe4244fddcc549f6777e15b6"
     )
   ]
 )
